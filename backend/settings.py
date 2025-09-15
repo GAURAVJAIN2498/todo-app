41834d01a8e13b2a907cf262740b9a21213ee4d9
Gaurav Jain <136182348+GAURAVJAIN2498@users.noreply.github.com>
2025-09-15 12:52:52 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -1,13 +1,10 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'todos',
+import os
+
+ALLOWED_HOSTS = [
+    "54.89.166.189",  # your EC2 public IP
+    "localhost",
+    "127.0.0.1"
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True  # allow React
+# If you later use a domain name, add it here too, e.g. "mytodoapp.com"
 
