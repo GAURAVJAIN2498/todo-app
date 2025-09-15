3d1f1b9a40642c5906594c66e4952d7d68565209
Gaurav Jain <136182348+GAURAVJAIN2498@users.noreply.github.com>
2025-09-15 13:10:11 +0530

    Update views.py

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework import viewsets
+from django.http import JsonResponse
 from .models import Todo
 from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+# Health endpoint
+def health(request):
+    return JsonResponse({"status": "ok"})
+
 
