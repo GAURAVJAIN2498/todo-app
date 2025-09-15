1586477865fc91fe755e76ba53c7e6eeba17a9a9
Gaurav Jain <136182348+GAURAVJAIN2498@users.noreply.github.com>
2025-09-15 13:09:23 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from todos.views import TodoViewSet
+from todos.views import TodoViewSet, health
 
 router = DefaultRouter()
 router.register(r'todos', TodoViewSet)
@@ -9,5 +9,6 @@ router.register(r'todos', TodoViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('health/', health),  # <-- Add this line
 ]
 
